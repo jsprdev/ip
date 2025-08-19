ef899eb68e539240ba21bed790ad51ccf2e83e27
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-08-19 18:09:54 +0800

    Level-0 commit

---
+++ src/main/java/Pichu.java
@@ -0,0 +1,11 @@
+public class Pichu {
+    public static void main(String[] args) {
+        String name = "Pichu";
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm " + name + "\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
