379ad8ed7a856ebf97f818aacbbded77c53f3ba7
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-08-22 02:49:41 +0800

    Level-2 commit

--- src/main/java/Pichu.java
+++ src/main/java/Pichu.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pichu {
@@ -10,18 +11,40 @@ public class Pichu {
                 "____________________________________________________________\n"
                 );
 
+        ArrayList<String> cache = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        while (true) {
+        boolean saidBye = false;
+        while (!saidBye) {
+
+
             String command = input.nextLine();
-            if (command.equals("bye")) {
-                System.out.println("____________________________________________________________\n" +
-                        " Bye. Hope to see you again soon!\n" +
-                        "___________________________________________________________");
-                break;
+
+            switch (command) {
+                case "bye":
+                    System.out.println("""
+                            ____________________________________________________________
+                             Bye. Hope to see you again soon!
+                            ___________________________________________________________""");
+                    saidBye = true;
+                    break;
+
+                case "list":
+                    System.out.println("____________________________________________________________\n ");
+                    for (int i = 0; i < cache.size(); i++) {
+                        System.out.println((i + 1) + ". " + cache.get(i));
+                    }
+                    System.out.println("\n___________________________________________________________");
+                    break;
+
+                default:
+                    cache.add(command);
+                    System.out.println("____________________________________________________________\n " +
+                            "added: " + command +
+                            "\n___________________________________________________________");
+                    break;
             }
-            System.out.println("____________________________________________________________\n " +
-                    command +
-                    "\n___________________________________________________________");
+
+
         }
 
     }
