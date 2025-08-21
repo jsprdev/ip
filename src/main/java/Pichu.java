27e0d1630c31f96b4a4d40c8d7d5d5a6429114b5
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-08-22 02:30:00 +0800

    Level-1 commit

--- src/main/java/Pichu.java
+++ src/main/java/Pichu.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Pichu {
     public static void main(String[] args) {
         String name = "Pichu";
+
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "___________________________________________________________");
+                "____________________________________________________________\n"
+                );
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "___________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________\n " +
+                    command +
+                    "\n___________________________________________________________");
+        }
+
     }
 }
