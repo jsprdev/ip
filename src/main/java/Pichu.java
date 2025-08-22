de0496d0e4130a46c76ac2a2c708aed77df004b8
Jasper Ang <127856665+jsprdev@users.noreply.github.com>
2025-08-22 14:14:42 +0800

    A-Enums commit

--- src/main/java/Pichu.java
+++ src/main/java/Pichu.java
@@ -127,6 +127,11 @@ public class Pichu {
                 continue;
             }
 
+            enum MainWord {
+                bye,
+                list
+            }
+
             switch (command) {
                 case "bye":
                     System.out.println("""
